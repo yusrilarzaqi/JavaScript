82c00b838d81c9e45b16885b63cdff6d7a7faa23
yusrilarzaqi <arzaqiyusril@gmail.com>
2023-02-07 14:09:32 +0700

    update 19 & 22

--- Node/WPU/19. Express Contact Bagian 3/app.js
+++ Node/WPU/19. Express Contact Bagian 3/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const {loadContact, findContact, addContact, cekDuplikat, deleteContact, updateContacts} = require('./utils/contacts');
 const {
-  body, validationResult, check 
-} = require('express-validator');
+	loadContact,
+	findContact,
+	addContact,
+	cekDuplikat,
+	deleteContact,
+	updateContacts,
+} = require('./utils/contacts');
+const { body, validationResult, check } = require('express-validator');
 const port = 8080;
 const app = express();
 
@@ -15,149 +20,155 @@ app.use(expressLayouts);
 
 // built-in middleware
 app.use(express.static('public'));
-pp.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-  const mahasiswa = [{
-    nama : "Yusril Arzaqi",
-    email : "arzaqiyusril@gmail.com"
-  },
-  {
-    nama : "Bimo Alam",
-    email : "Bimoal@gmail.com"
-  },
-  {
-    nama : "Dimas Rafif",
-    email : "DimsRaf@gmail.com"
-  }];
-  res.render('index', { 
-    layout: 'layouts/main-layout',
-    nama: 'Yusril Arzaqi',
-    title : "NodeJS Web Server",
-    mahasiswa,
-  });
+	const mahasiswa = [
+		{
+			nama: 'Yusril Arzaqi',
+			email: 'arzaqiyusril@gmail.com',
+		},
+		{
+			nama: 'Bimo Alam',
+			email: 'Bimoal@gmail.com',
+		},
+		{
+			nama: 'Dimas Rafif',
+			email: 'DimsRaf@gmail.com',
+		},
+	];
+	res.render('index', {
+		layout: 'layouts/main-layout',
+		nama: 'Yusril Arzaqi',
+		title: 'NodeJS Web Server',
+		mahasiswa,
+	});
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', {
-    layout: 'layouts/main-layout',
-    title : 'Halaman About',
-  });
+	res.render('about', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman About',
+	});
 });
 
-
 app.get('/contact', (req, res) => {
-  const contacts = loadContact();
-  res.render('contact', {layout: 'layouts/main-layout',title : 'Halaman Contact',contacts,});
+	const contacts = loadContact();
+	res.render('contact', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman Contact',
+		contacts,
+	});
 });
 
 app.get('/contact/add', (req, res) => {
-  res.render('add-contact', {
-    layout: 'layouts/main-layout',
-    title : 'From Tambah Data Contact',
-  });
+	res.render('add-contact', {
+		layout: 'layouts/main-layout',
+		title: 'From Tambah Data Contact',
+	});
 });
 
-app.post('/contact',[
-  body('nama').custom(value => {
-    const duplikat = cekDuplikat(value);
-    if (duplikat){
-      throw new Error(`Nama ${value} sudah ada, cari yang lain!`);
-    }
-    return true;
-  }),
-  check('email', 'Email Tidak Valid').isEmail(),
-  check('noHp', 'No Hp Tidak Valid').isMobilePhone('id-ID')
-], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.render('add-contact',{
-      title : 'From Tambah Data Contact',
-      layout: 'layouts/main-layout',
-      errors: errors.array()
-    });
-  } else {
-    addContact(req.body);
-    
-    //kirimkan flash
-    res.redirect('/contact');
-  }
-});
+app.post(
+	'/contact',
+	[
+		body('nama').custom((value) => {
+			const duplikat = cekDuplikat(value);
+			if (duplikat) {
+				throw new Error(`Nama ${value} sudah ada, cari yang lain!`);
+			}
+			return true;
+		}),
+		check('email', 'Email Tidak Valid').isEmail(),
+		check('noHp', 'No Hp Tidak Valid').isMobilePhone('id-ID'),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.render('add-contact', {
+				title: 'From Tambah Data Contact',
+				layout: 'layouts/main-layout',
+				errors: errors.array(),
+			});
+		} else {
+			addContact(req.body);
+
+			//kirimkan flash
+			res.redirect('/contact');
+		}
+	}
+);
 
 // process delete contact
 app.get('/contact/delete/:nama', (req, res) => {
-  const contact = findContact(req.params.nama);
+	const contact = findContact(req.params.nama);
 
-  // jika contact tidak ada 
-  if (!contact){ 
-    res.status(404);
-    res.send('<h1>404</h1>');
-  } else {
-    deleteContact(req.params.nama)
-  }
-  res.redirect('/contact')
+	// jika contact tidak ada
+	if (!contact) {
+		res.status(404);
+		res.send('<h1>404</h1>');
+	} else {
+		deleteContact(req.params.nama);
+	}
+	res.redirect('/contact');
 });
 
 // Halamabn Form Update contact
 app.get('/contact/edit/:nama', (req, res) => {
-  const contact = findContact(req.params.nama);
-  res.render('edit-contact', {
-    layout: 'layouts/main-layout',
-    title : 'From Ubah Data Contact',
-    contact,
-  });
+	const contact = findContact(req.params.nama);
+	res.render('edit-contact', {
+		layout: 'layouts/main-layout',
+		title: 'From Ubah Data Contact',
+		contact,
+	});
 });
 
 app.post(
-  '/contact/update',
-  [
-    body('nama').custom((value, { req  }) => {
-      const duplikat = cekDuplikat(value);
-      if (duplikat || value !== req.body.nama){
-      // if ((value !==  req.body.nama) && (duplikat)){
-        throw new Error(`Nama ${value} sudah ada, cari yang lain!`);
-      }
-      return true;
-  }),
-    check('email', 'Email Tidak Valid').isEmail(),
-    check('noHp', 'No Hp Tidak Valid').isMobilePhone('id-ID')
-  ], (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.render('edit-contact',{
-      title : 'From Ubah Data Contact',
-      layout: 'layouts/main-layout',
-      errors: errors.array(),
-      contact: req.body,
-    });
-  } else {
-    updateContacts(req.body);
-    
-    //kirimkan flash
-    res.redirect('/contact');
-  }
-});
+	'/contact/update',
+	[
+		body('nama').custom((value, { req }) => {
+			const duplikat = cekDuplikat(value);
+			if (duplikat || value !== req.body.nama) {
+				// if ((value !==  req.body.nama) && (duplikat)){
+				throw new Error(`Nama ${value} sudah ada, cari yang lain!`);
+			}
+			return true;
+		}),
+		check('email', 'Email Tidak Valid').isEmail(),
+		check('noHp', 'No Hp Tidak Valid').isMobilePhone('id-ID'),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			res.render('edit-contact', {
+				title: 'From Ubah Data Contact',
+				layout: 'layouts/main-layout',
+				errors: errors.array(),
+				contact: req.body,
+			});
+		} else {
+			updateContacts(req.body);
 
-
-
+			//kirimkan flash
+			res.redirect('/contact');
+		}
+	}
+);
 
 app.get('/contact/:nama', (req, res) => {
-  const contact = findContact(req.params.nama);
-  
-  res.render('detail', {
-    layout: 'layouts/main-layout',
-    title : 'Halaman Detail Contact',
-    contact,
-  });
+	const contact = findContact(req.params.nama);
+
+	res.render('detail', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman Detail Contact',
+		contact,
+	});
 });
 
-
 app.use((req, res) => {
-  res.status(404);
-  res.send("<h1>404: Page Not Found</h1>");
-})
+	res.status(404);
+	res.send('<h1>404: Page Not Found</h1>');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-})
-
+	console.log(`Example app listening at http://localhost:${port}`);
+});
--- Node/WPU/22. MongoDB & NodeJS/app.js
+++ Node/WPU/22. MongoDB & NodeJS/app.js
@@ -1,18 +1,80 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
-const URI =
-	'mongodb+srv://yusrilarzaqi:yusril145@cluster0.rbh4y5t.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://yusrilarzaqi:<password>@cluster0.rbh4y5t.mongodb.net/?retryWrites=true&w=majority`;
 const dbName = 'wpu';
-const client = new MongoClient(URI);
+const client = new MongoClient(uri);
 
+// Async Await
 async function run() {
 	try {
 		const database = client.db(dbName);
 		const mahasiswa = database.collection('mahasiswa');
-		const query = { nama: 'Irfan Ardianto' };
-		const mhs = await mahasiswa.findOne(query);
 
-		console.log(mhs);
+		// insert 1 data to collection
+		/* await mahasiswa.insertOne({ */
+		/* 	nama: 'Yusril Arzaqi', */
+		/* 	email: 'arzaqiyusril', */
+		/* }); */
+
+		// insert many data to collection
+		/* const mhs = await mahasiswa.insertMany([ */
+		/* 	{ */
+		/* 		nama: 'Yusril Arzaqi', */
+		/* 		email: 'arzaqiyusril', */
+		/* 	}, */
+		/* 	{ */
+		/* 		nama: 'Bimo Alamsyah', */
+		/* 		email: 'bimo@gmail.com', */
+		/* 	}, */
+		/* ]); */
+
+		// menampilkan semua data di collection
+		/* const datas = await mahasiswa.find().toArray(); */
+		/* datas.map((mhs, i) => { */
+		/* 	console.log(`${i}. ${mhs.nama}:${mhs.email}`); */
+		/* }); */
+
+		// menampilkan semua data di collection dengan kriteria
+		/* const datas = await mahasiswa */
+		/* 	.find({ */
+		/* 		_id: new ObjectId('63dfa1518ab8c9482747a31e'), */
+		/* 	}) */
+		/* 	.toArray(); */
+		/* console.log(datas); */
+
+		// mengubah data berdasarkan id (1)
+		/* const result = await mahasiswa.updateOne( */
+		/* 	{ */
+		/* 		_id: new ObjectId('63dfa1518ab8c9482747a31e'), */
+		/* 	}, */
+		/* 	{ */
+		/* 		$set: { email: 'arzaqiyusril@gmail.com' }, */
+		/* 	} */
+		/* ); */
+		/* console.log( */
+		/* 	`${result.matchedCount} document(s) matched the filter, updated ${result.upsertedCount} document(s)` */
+		/* ); */
+
+		// mengubah data lebih dari satu menggunakan field
+		/* const result = await mahasiswa.updateMany( */
+		/* 	{ nama: 'Yusril Arzaqi' }, */
+		/* 	{ $set: { email: 'arzaqiyusril@gmail.com' } } */
+		/* ); */
+		/* console.log( */
+		/* 	`${result.matchedCount} document(s) matched the filter, updated ${result.upsertedCount} document(s)` */
+		/* ); */
+
+		// menghapus 1 data
+		/* const result = await mahasiswa.deleteOne({ nama: 'yusril' }); */
+		/* if (result.deletedCount == 1) { */
+		/* 	console.log('successfully deleted'); */
+		/* } else { */
+		/* 	console.log('no data deleted'); */
+		/* } */
+
+		// menghapus  lebih dari 1 data
+		const result = await mahasiswa.deleteMany({ nama: { $regex: 'Yusril' } });
+		console.log(`Deleted ${result.deletedCount} data`);
 	} finally {
 		await client.close();
 	}
